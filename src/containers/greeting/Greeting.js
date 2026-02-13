3d42f6521ef86cdc824e0272b1ea1ca53814fe15
Sheikh Wasimuddin <161950279+sheikhwasimuddin@users.noreply.github.com>
2026-02-13 15:12:39 +0530

    Update resume link to new PDF file

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -41,7 +41,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
+                    href={require("./sheikhwasimuddin_resume.pdf")}
                     download="Resume.pdf"
                     className="download-link-button"
                   >
